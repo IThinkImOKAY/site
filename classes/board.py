d50472e981edae16c4feee7c4326442c1fea630f
ax3cutor <throwaway.546372819@protonmail.com>
2021-05-05 23:04:16 +0200

    update boards

--- classes/board.py
+++ classes/board.py
@@ -1,5 +1,6 @@
 from sqlalchemy import *
 from __main__ import Base
+import time
 
 class Board(Base):
 	__tablename__ = "Boards"
@@ -9,3 +10,9 @@ class Board(Base):
 	description = Column(String(255))
 	created_utc = Column(Integer)
 	creation_ip = Column(String(255))
+
+	def __init__(self, **kwargs):
+		if 'created_utc' not in kwargs:
+			kwargs['created_utc'] = int(time.time())
+
+		super().__init__(**kwargs)
