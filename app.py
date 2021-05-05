4278502e2fbbcfb1db8fda1be0d38a6d07a07207
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-05-05 10:15:15 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,17 +5,33 @@ app = Flask(__name__,static_url_path='')
 with open('main.yaml') as maindb:
     config = yaml.safe_load(maindb)
 
+def dump_it(data):
+    with open('main.yaml','w') as cf:
+        yaml.dump(data,cf)
+    return 'dumped!'
+
 @app.route('/', methods=['GET'])
 def slash():
     boards = config['boards']
     communities = [config['boards'][c]['id'] for c in config['boards']]
     return render_template('index.html',communities=communities)
 
-#@app.route('/create_board', methods=['GET'])
-#def get_create_board():
-#    return render_template('create.html',)
+@app.route('/create_board', methods=['GET'])
+def get_create_board():
+    return render_template('create.html',)
 
-@app.route('/c/<cid>/')
+@app.route('/create_board', methods=['POST'])
+def post_create_board():
+    bid = request.form['id']
+    bname = request.form['name']
+    if config['boards'].get(bid):
+        return 'that board already exists!'
+    else:
+        config['boards'][bid] = {'id':bid,'name':bname}
+        dump_it(data=config)
+        return redirect('/c/{}'.format(bid))
+    
+@app.route('/c/<cid>/')           
 def c(cid):
     if config['boards'].get(cid):
         return render_template('board.html',title='/c/'+cid,cname=config['boards'][cid]['name'])
