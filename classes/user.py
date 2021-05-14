c034c5556664b147e3872e7d98dc79d268554519
fireworks88 <throwaway.546372819@protonmail.com>
2021-05-15 00:24:18 +0200

    formkey property

--- classes/user.py
+++ classes/user.py
@@ -1,39 +1,52 @@
 from sqlalchemy import *
 from sqlalchemy.orm import relationship
 from __main__ import Base
+from flask import session
 from helpers.hash import *
 import time
+import secrets
 
 class User(Base):
-	__tablename__ = "Users"
+    __tablename__ = "Users"
 
-	id = Column(Integer, Sequence('Users_id_seq'), primary_key = True)
-	username = Column(String(50))
-	passhash = Column(String(750))
-	created_utc = Column(Integer)
-	creation_ip = Column(String(255))
-	is_admin = Column(Boolean, default = False)
-	banned_utc = Column(Integer, default = 0)
-	ban_expires_utc = Column(Integer, default = 0)
-	banned_by_id = Column(Integer, ForeignKey('Users.id'))
-	deleted_utc = Column(Integer, default = 0)
+    id = Column(Integer, Sequence('Users_id_seq'), primary_key = True)
+    username = Column(String(50))
+    passhash = Column(String(750))
+    created_utc = Column(Integer)
+    creation_ip = Column(String(255))
+    is_admin = Column(Boolean, default = False)
+    banned_utc = Column(Integer, default = 0)
+    ban_expires_utc = Column(Integer, default = 0)
+    banned_by_id = Column(Integer, ForeignKey('Users.id'))
+    deleted_utc = Column(Integer, default = 0)
 
-	def __init__(self, **kwargs):
-		if 'created_utc' not in kwargs:
-			kwargs['created_utc'] = int(time.time())
+    def __init__(self, **kwargs):
+        if 'created_utc' not in kwargs:
+            kwargs['created_utc'] = int(time.time())
 
-		if 'password' in kwargs:
-			kwargs['passhash'] = hash_password(kwargs['password'])
-			kwargs.pop('password', None)
+        if 'password' in kwargs:
+            kwargs['passhash'] = hash_password(kwargs['password'])
+            kwargs.pop('password', None)
 
-		super().__init__(**kwargs)
+        super().__init__(**kwargs)
 
-	def __repr__(self):
-		return f"<User(id='{self.id}'; name='{self.username}')>"
+    def __repr__(self):
+        return f"<User(id='{self.id}'; name='{self.username}')>"
 
-	@property
-	def is_banned(self):
-		return self.banned_utc > 0
+    @property
+    def formkey(self):
+        if 'session_id' not in session:
+            session['session_id'] = secrets.token_hex(16)
 
-	def verify_password(self, password) -> bool:
-		return check_password(password, self.passhash)
+        _formkey = f"{self.id}#{session['session_id']}"
+
+        _formkey = hash(_formkey)
+        return _formkey
+
+
+    @property
+    def is_banned(self):
+        return self.banned_utc > 0
+
+    def verify_password(self, password) -> bool:
+        return check_password(password, self.passhash)
