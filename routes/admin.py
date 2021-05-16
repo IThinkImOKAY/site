d733fdf9373d74712575e27b622612867618e3f2
fireworks88 <throwaway.546372819@protonmail.com>
2021-05-16 22:49:30 +0200

    delete comments

--- routes/admin.py
+++ routes/admin.py
@@ -3,7 +3,7 @@ from flask import g, redirect
 from helpers.get import *
 from helpers.wrappers import *
 
-@app.post('/admin/remove/post/<int:pid>')
+@app.post('/admin/remove/<int:pid>')
 @auth_required
 @admin_required
 @validate_formkey
@@ -16,20 +16,8 @@ def admin_remove_post(pid, u):
 
     cache.delete_memoized(target.board.post_list)
 
-    return redirect(target.permalink)
-
-@app.post('/admin/remove/comment/<int:cid>')
-@auth_required
-@admin_required
-@validate_formkey
-def admin_remove_comment(cid, u):
-    target = get_comment(cid, graceful = False)
-
-    reason = request.form.get("reason", "")
-
-    target.remove(reason = reason)
-
-    cache.delete_memoized(target.parent.comment_list)
+    if not target.is_top_level:
+        cache.delete_memoized(target.parent.comment_list)
 
     return redirect(target.permalink)
 
@@ -46,7 +34,7 @@ def admin_ban_board(bid, u):
 
     return redirect(target.url)
 
-@app.post('/admin/approve/post/<int:pid>')
+@app.post('/admin/approve/<int:pid>')
 @auth_required
 @admin_required
 @validate_formkey
@@ -63,24 +51,8 @@ def admin_approve_post(pid, u):
 
     cache.delete_memoized(target.board.post_list)
 
-    return redirect(target.permalink)
-
-@app.post('/admin/approve/comment/<int:cid>')
-@auth_required
-@admin_required
-@validate_formkey
-def admin_approve_comment(cid, u):
-    target = get_comment(cid, graceful = False)
-
-    if not target.is_removed:
-        abort(400)
-
-    target.is_removed = False
-    target.removal_reason = ""
-
-    g.db.add(target)
-
-    cache.delete_memoized(target.parent.comment_list)
+    if not target.is_top_level:
+        cache.delete_memoized(target.parent.comment_list)
 
     return redirect(target.permalink)
 
