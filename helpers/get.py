2152bb7a6bfc070418d25e89cbbe95607efcfb5b
fireworks88 <throwaway.546372819@protonmail.com>
2021-06-24 22:56:50 +0200

    indents

--- helpers/get.py
+++ helpers/get.py
@@ -4,56 +4,56 @@ from classes.post import *
 from classes.user import *
 
 def get_board(boardname, graceful = True) -> Board:
-	board = g.db.query(Board).filter_by(name = boardname).first()
+    board = g.db.query(Board).filter_by(name = boardname).first()
 
-	if not board:
-		if not graceful:
-			abort(404)
-		else:
-			return None
+    if not board:
+        if not graceful:
+            abort(404)
+        else:
+            return None
 
-	return board
+    return board
 
 def get_board_id(bid, graceful = True) -> Board:
-	board = g.db.query(Board).filter_by(id = bid).first()
+    board = g.db.query(Board).filter_by(id = bid).first()
 
-	if not board:
-		if not graceful:
-			abort(404)
-		else:
-			return None
+    if not board:
+        if not graceful:
+            abort(404)
+        else:
+            return None
 
-	return board
+    return board
 
 def get_post(pid, graceful = True) -> Post:
-	post = g.db.query(Post).filter_by(id = pid).first()
+    post = g.db.query(Post).filter_by(id = pid).first()
 
-	if not post:
-		if not graceful:
-			abort(404)
-		else:
-			return None
+    if not post:
+        if not graceful:
+            abort(404)
+        else:
+            return None
 
-	return post
+    return post
 
 def get_user(username, graceful = True) -> User:
-	user = g.db.query(User).filter_by(username = username).first()
+    user = g.db.query(User).filter_by(username = username).first()
 
-	if not user:
-		if not graceful:
-			abort(404)
-		else:
-			return None
+    if not user:
+        if not graceful:
+            abort(404)
+        else:
+            return None
 
-	return user
+    return user
 
 def get_user_id(uid, graceful = True) -> User:
-	user = g.db.query(User).filter_by(id = uid).first()
+    user = g.db.query(User).filter_by(id = uid).first()
 
-	if not user:
-		if not graceful:
-			abort(404)
-		else:
-			return None
+    if not user:
+        if not graceful:
+            abort(404)
+        else:
+            return None
 
-	return user
+    return user
