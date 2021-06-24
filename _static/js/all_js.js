cf476cf85beab6d6637a329ce4cb2711e8461df5
fireworks88 <throwaway.546372819@protonmail.com>
2021-06-24 22:58:27 +0200

    favorites

--- _static/js/all_js.js
+++ _static/js/all_js.js
@@ -22,6 +22,44 @@ const quote = (pid) => {
 	reply.value += `>>${pid} `;
 };
 
+const toggleFavorite = (board) => {
+
+	let formData = new FormData();
+	formData.append("board", `/${board}/`);
+
+	let xhr = new XMLHttpRequest();
+	xhr.responseType = "json";
+	xhr.withCredentials = true;
+
+	xhr.open("POST", "/toggle-favorite");
+	xhr.send(formData);
+
+	xhr.onload = () => {
+		const h = document.getElementById("header-boardlist");
+		const m = document.getElementById("mobile-boardlist");
+
+		if (xhr.response.length > 0) {
+			h.innerHTML = `[  ${xhr.response.map((b) => `<a href="${b}">${b}</a>`).join('  ')}  ]`;
+		} else {
+			h.innerHTML = '';
+		}
+
+		m.innerHTML = `
+			${xhr.response.map((b) => `
+				<li>
+					<a href="${b}">${b}</a>
+				</li>
+			`).join(' ')}`;
+
+		const btn = document.getElementById("toggle-fav");
+		btn.classList.toggle('text-yellow-500');
+		btn.classList.toggle('text-muted');
+	};
+
+	xhr.onerror = () => alert("Failed to save settings.");
+
+};
+
 document.addEventListener('click', e => {
     if (e.target.closest("#inner")) {
         closeNav();
